72be40437df29d38a0f423737ae8bd6d7b320325
Artem V. Andreev <artem@iling.spb.ru>
2017-01-22 15:46:51 +0300

    changed license terms

--- ast.h
+++ ast.h
@@ -1,21 +1,19 @@
-/*
- * Copyright (c) 2016  Artem V. Andreev
- * This file is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
- *
- * This file is distributed in the hope that it will be useful,
+/**************************************************************************
+ * Copyright (c) 2017 Artem V. Andreev
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
- *
- */
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+**************************************************************************/
 /** @file
  * @brief Abstract Syntax Tree
  *
